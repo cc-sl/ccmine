c8632010f335aa22a3529ccbbd356316da6a5325
cc-sl <2703023005@qq.com>
2026-05-25 20:38:56 +0800

    feat: 添加基础类的定义

---
+++ config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+MAX_WIDTH = 10
+MAX_HEIGHT = 10
+MAX_MINES = 20
+MAX_FLAGS = 20
---
+++ the_point.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+'''
+基础类定义
+包括
+1. ThePoint：表示一个点的位置和周围的点
+2. TheMine：表示一个格子是否有雷
+3. TheFlag：表示一个格子标记状态，包括无标记、插旗、问号
+'''
+from config import *
+class ThePoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.pos = 'in'
+        self.mine = False
+        self.flag = 'none'
+        if x<0 or x>MAX_WIDTH-1 or y<0 or y>MAX_HEIGHT-1:
+            self.pos = 'out'
+        if x==0 and y==0:
+            self.pos = 'left-top'
+        if x==0 and y==MAX_HEIGHT-1:
+            self.pos = 'left-bottom'
+        if x==MAX_WIDTH-1 and y==0:
+            self.pos = 'right-top'
+        if x==MAX_WIDTH-1 and y==MAX_HEIGHT-1:
+            self.pos = 'right-bottom'
+
+        if x==0 and y>0 and y<MAX_HEIGHT-1:
+            self.pos = 'left'
+        if x==MAX_WIDTH-1 and y>0 and y<MAX_HEIGHT-1:
+            self.pos = 'right'
+        if y==0 and x>0 and x<MAX_WIDTH-1:
+            self.pos = 'top'
+        if y==MAX_HEIGHT-1 and x>0 and x<MAX_WIDTH-1:
+            self.pos = 'bottom'
+
+    def get_position(self):
+        return self.pos
+
+    def get_around(self):
+        if self.pos == 'in':
+            return [(self.x-1, self.y-1), (self.x, self.y-1), (self.x+1, self.y-1),
+                    (self.x-1, self.y),                 (self.x+1, self.y),
+                    (self.x-1, self.y+1), (self.x, self.y+1), (self.x+1, self.y+1)] 
+        elif self.pos == 'left-top':
+            return [(self.x, self.y+1), (self.x+1, self.y), (self.x+1, self.y+1)]
+        elif self.pos == 'left-bottom':
+            return [(self.x, self.y-1), (self.x+1, self.y), (self.x+1, self.y-1)]
+        elif self.pos == 'right-top':
+            return [(self.x-1, self.y), (self.x-1, self.y+1), (self.x, self.y+1)]
+        elif self.pos == 'right-bottom':
+            return [(self.x-1, self.y), (self.x-1, self.y-1), (self.x, self.y-1)]
+        elif self.pos == 'left':
+            return [(self.x, self.y-1), (self.x+1, self.y-1), (self.x+1, self.y), (self.x+1, self.y+1), (self.x, self.y+1)]
+        elif self.pos == 'right':
+            return [(self.x-1, self.y-1), (self.x-1, self.y), (self.x-1, self.y+1), (self.x, self.y+1), (self.x, self.y-1)]
+        elif self.pos == 'top':
+            return [(self.x-1, self.y), (self.x-1, self.y+1), (self.x, self.y+1), (self.x+1, self.y+1), (self.x+1, self.y)]
+        elif self.pos == 'bottom':
+            return [(self.x-1, self.y), (self.x-1, self.y-1), (self.x, self.y-1), (self.x+1, self.y-1), (self.x+1, self.y)]
+
+if __name__ == "__main__":
+    for y in range(-1, MAX_WIDTH+1):
+        for x in range(-1, MAX_HEIGHT+1):
+            p = ThePoint(x, y)
+            print(f"{p.get_position()}",end='\t')
+        print()
+
+    
